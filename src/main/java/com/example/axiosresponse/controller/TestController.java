aa21916a49787af969cd5cc616ddf0d4ec5652f6
Doha <adamdoha@naver.com>
2022-03-19 19:46:49 +0900

    CORS

--- src/main/java/com/example/axiosresponse/controller/TestController.java
+++ src/main/java/com/example/axiosresponse/controller/TestController.java
@@ -2,11 +2,13 @@ package com.example.axiosresponse.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequestMapping("/api/v1/test")
 @RestController
 public class TestController {
